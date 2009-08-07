a16b31b20346d083229f09b7219972beab5226ee
fabio.fantato <fabio.fantato@337e1542-b571-11dd-8d2d-9519526c4844>
2009-08-07 19:34:09 +0000

    build001:279

--- trunk/ivela-web/src/main/java/br/ufc/ivela/servlets/IRCIvelaClientServlet.java
+++ trunk/ivela-web/src/main/java/br/ufc/ivela/servlets/IRCIvelaClientServlet.java
@@ -5,8 +5,6 @@
 
 package br.ufc.ivela.servlets;
 
-import br.ufc.ivela.commons.model.SystemUser;
-import br.ufc.ivela.ejb.interfaces.SystemUserRemote;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,24 +14,29 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.context.SecurityContextHolder;
+
+import br.ufc.ivela.commons.model.SystemUser;
+import br.ufc.ivela.ejb.interfaces.SystemUserRemote;
 
 /**
  *
+ * @author jefferson
  * @author jdamico
  */
 public class IRCIvelaClientServlet extends HttpServlet{
 
-	private static final long serialVersionUID = -4341295579947052433L;
+    private static final long serialVersionUID = -4341295579947052433L;
 
-	@Override
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     
         Long oid = 0L;
         Object obj = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (obj != null && obj instanceof SystemUser) {
-            oid = ((SystemUser) obj).getId();
+            oid = ((SystemUser) obj).getId();            
         }
         String email = (String)request.getSession().getAttribute("USER_EMAIL");
         
@@ -42,7 +45,7 @@ public class IRCIvelaClientServlet extends HttpServlet{
             SystemUserRemote systemUserRemote = getUserVoiceRemote();
             if(systemUserRemote!=null){
                SystemUser systemUser = systemUserRemote.get(oid);
-               email = systemUser.getEmail();
+               email = systemUser.getEmail();               
                request.getSession().putValue("USER_EMAIL", email);
             }
         }
@@ -52,28 +55,27 @@ public class IRCIvelaClientServlet extends HttpServlet{
         for(int i=0; i < email.length(); i++){
             if(email.charAt(i) == at.charAt(0)) pos = i;
         }
-        email = email.substring(0,pos);
+        email = email.substring(0,pos);        
         response.setCharacterEncoding("ISO-8859-1");
         PrintWriter out = response.getWriter();
-        out.println(   "<html>\n" +
-                                        "<head>\n" +
-                                        "<title>IRC Ivela Client - #room_dis"+request.getParameter("discipline.id")+"_course"+request.getParameter("course.id")+"</title>\n" +
-                                        "</head>\n" +
-                                        "<body>\n" +
-                                        "<h1>IRC Ivela Client - #room_dis"+request.getParameter("discipline.id")+"_course"+request.getParameter("course.id")+"</h1><hr>\n" +
-                                        "<applet code=\"org.jdamico.ircivelaclient.view.HandleApplet\"\n" +
-                                        "archive=\"http://"+request.getServerName()+"/public_content/ircivelaclient/ircivelaclient.jar\" \n" +
-                                        "width=\"820\" height=\"480\">\n" +
-                                        "<param name=\"server\" value=\""+request.getServerName()+"\" >\n" +
-                                        "<param name=\"channel\" value=\"#room_dis"+request.getParameter("discipline.id")+"_course"+request.getParameter("course.id")+"\" >\n" +
-                                        "<param name=\"nick\" value=\""+email+"\" >\n" +
-                                        "<param name=\"teacher\" value=\"damico\" >\n" +
-                                        "<param name=\"bgcolor\" value=\"FDF1D9\" >" +
-                                        "</applet> \n" +
-                                        "</body>\n" +
-                                        "</html>\n");
+        out.println("<html>\n" +
+                    "<head>\n" +
+                    "<title>IRC Ivela Client - #room_dis"+request.getParameter("discipline.id")+"_course"+request.getParameter("course.id")+"</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "<h1>IRC Ivela Client - #room_dis"+request.getParameter("discipline.id")+"_course"+request.getParameter("course.id")+"</h1><hr>\n" +
+                    "<applet code=\"org.jdamico.ircivelaclient.view.HandleApplet\"\n" +
+                    "archive=\"http://"+request.getServerName()+"/public_content/ircivelaclient/ircivelaclient.jar\" \n" +
+                    "width=\"820\" height=\"480\">\n" +
+                    "<param name=\"server\" value=\""+request.getServerName()+"\" >\n" +
+                    "<param name=\"channel\" value=\"#room_dis"+request.getParameter("discipline.id")+"_course"+request.getParameter("course.id")+"\" >\n" +
+                    "<param name=\"nick\" value=\""+email+"\" >\n" +
+                    "<param name=\"teacher\" value=\"damico\" >\n" +
+                    "<param name=\"bgcolor\" value=\"FDF1D9\" >" +
+                    "</applet> \n" +
+                    "</body>\n" +
+                    "</html>\n");        
         out.close();
-        
     }
     
     private SystemUserRemote getUserVoiceRemote() {
