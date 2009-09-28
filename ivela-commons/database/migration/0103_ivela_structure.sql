7714fb9bef78d02bdcb6c99601447b4497dd9029
rafael.lagoa <rafael.lagoa@337e1542-b571-11dd-8d2d-9519526c4844>
2009-09-28 17:39:00 +0000

    1244 - [Student] Progress controller

--- ivela-commons/database/migration/0103_ivela_structure.sql
+++ ivela-commons/database/migration/0103_ivela_structure.sql
@@ -14,5 +14,23 @@ ALTER TABLE file ADD COLUMN grade integer DEFAULT 0;
 ALTER TABLE topic ADD COLUMN posts_count integer NOT NULL DEFAULT 0;
 ALTER TABLE topic ADD COLUMN last_post_id integer;
 ALTER TABLE topic ADD COLUMN last_post_date timestamp without time zone;
-
-
+CREATE TABLE grade_unit_content (
+    id numeric(10,0) NOT NULL,
+    grade numeric(10,0) NOT NULL,
+    unit_content numeric(10,0) NOT NULL,
+    start_datetime timestamp without time zone,
+    mail_flag smallint DEFAULT 1
+);
+ALTER TABLE ivela.grade_unit_content OWNER TO ivela;
+CREATE SEQUENCE sq_grade_unit_content
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+ALTER TABLE ivela.sq_grade_unit_content OWNER TO ivela;
+ALTER SEQUENCE sq_grade_unit_content OWNED BY grade_unit_content.id;
+ALTER TABLE grade_unit_content ALTER COLUMN id SET DEFAULT nextval('sq_grade_unit_content'::regclass);
+ALTER TABLE ONLY grade_unit_content ADD CONSTRAINT pk_grade_unit_content PRIMARY KEY (id);
+ALTER TABLE ONLY grade_unit_content ADD CONSTRAINT grade_user_grade_unit_content_fk FOREIGN KEY (grade) REFERENCES grade(id);
+ALTER TABLE ONLY grade_unit_content ADD CONSTRAINT unit_content_user_grade_unit_content_fk FOREIGN KEY (unit_content) REFERENCES unit_content(id);
