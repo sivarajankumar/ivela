416cf0871276031379e6ce561fd55ef0beb3d2dc
fabio.fantato <fabio.fantato@337e1542-b571-11dd-8d2d-9519526c4844>
2009-10-14 23:53:38 +0000

    [1244] table of contents

--- branches/issue017/ivela-commons/database/ivela.sql
+++ branches/issue017/ivela-commons/database/ivela.sql
@@ -5925,6 +5925,67 @@ REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
+--
+-- Start Content Modifications
+--
+DELETE from Attach_Post;
+DELETE from file;
+DELETE from post;
+DELETE from topic;
+DELETE from forum;
+ALTER TABLE forum ALTER COLUMN student_create_topic SET DEFAULT true;
+ALTER TABLE forum ALTER COLUMN student_upload_post SET DEFAULT true;
+
+CREATE TABLE grade_unit_content (
+    id numeric(10,0) NOT NULL,
+    grade numeric(10,0) NOT NULL,
+    unit_content numeric(10,0) NOT NULL,
+    start_datetime timestamp without time zone,
+    finish_datetime timestamp without time zone,
+    mail_flag boolean
+);
+ALTER TABLE ivela.grade_unit_content OWNER TO ivela;
+CREATE SEQUENCE sq_grade_unit_content
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+ALTER TABLE ivela.sq_grade_unit_content OWNER TO ivela;
+ALTER SEQUENCE sq_grade_unit_content OWNED BY grade_unit_content.id;
+ALTER TABLE grade_unit_content ALTER COLUMN id SET DEFAULT nextval('sq_grade_unit_content'::regclass);
+ALTER TABLE ONLY grade_unit_content ADD CONSTRAINT pk_grade_unit_content PRIMARY KEY (id);
+ALTER TABLE ONLY grade_unit_content ADD CONSTRAINT grade_user_grade_unit_content_fk FOREIGN KEY (grade) REFERENCES grade(id);
+ALTER TABLE ONLY grade_unit_content ADD CONSTRAINT unit_content_user_grade_unit_content_fk FOREIGN KEY (unit_content) REFERENCES unit_content(id);
+
+ALTER TABLE unit_content ADD COLUMN duration time without time zone;
+UPDATE unit_content SET duration = '00:00:00';
+
+ALTER TABLE course ADD COLUMN custom_toc boolean DEFAULT false;
+
+ALTER TABLE challenge_items ADD COLUMN weight integer NOT NULL DEFAULT 1;
+ALTER TABLE challenge ADD COLUMN unit_id integer NOT NULL DEFAULT 0;
+ALTER TABLE challenge ADD COLUMN retries smallint NOT NULL DEFAULT 0;
+ALTER TABLE course DROP COLUMN challenge_itens_enabled;
+ALTER TABLE course ADD COLUMN challenge_retries smallint NOT NULL DEFAULT 0;
+ALTER TABLE course ADD COLUMN challenge_count smallint NOT NULL DEFAULT 0;
+ALTER TABLE course ADD COLUMN challenge_weight integer NOT NULL DEFAULT 0;
+CREATE INDEX index_challenge_by_name_course ON challenge_items(course, name);
+ALTER TABLE transcript ADD COLUMN average_challenge numeric(7,2) DEFAULT 0;
+ALTER TABLE transcript ADD COLUMN challenges_done smallint DEFAULT 0;
+ALTER TABLE transcript ADD COLUMN challenges_weight integer DEFAULT 0;
+ALTER TABLE transcript ADD COLUMN challenges_total numeric(10,2) DEFAULT 0;
+UPDATE challenge_items set weight=1 where name LIKE '%';
+
+ALTER TABLE discipline ADD COLUMN tag character varying(20);
+ALTER TABLE unit_content ADD COLUMN tag character varying(20);
+
+UPDATE discipline SET tag = SUBSTRING(LOWER(REPLACE(name, ' ', '')),1,20);
+UPDATE unit_content SET tag = SUBSTRING(LOWER(REPLACE(title, ' ', '')),1,20);
+--- End of Content Modifications
+
+
+
 
 --
 -- PostgreSQL database dump complete
