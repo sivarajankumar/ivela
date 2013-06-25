4de586f2f5ee9aae8bed6ef24f2d19fbdc5d6e27
jd.comment <jd.comment@337e1542-b571-11dd-8d2d-9519526c4844>
2013-06-25 03:44:44 +0000

    Starting IE and Jetty in Threads

--- trunk/IvelaOffline/src/org/ivela/offline/jetty/StartJetty.java
+++ trunk/IvelaOffline/src/org/ivela/offline/jetty/StartJetty.java
@@ -17,14 +17,16 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.ivela.offline.servlets.JsHelper;
 
 /**
- *  Classe principal que configura e inicia o Jetty
- *  
+ * Classe principal que configura e inicia o Jetty
+ * 
  * @author julianom
- *
+ * 
  */
-public class StartJetty extends AbstractHandler {
+public class StartJetty extends AbstractHandler implements Runnable {
 
-	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	public void handle(String target, Request baseRequest,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		response.setContentType("text/html;charset=utf-8");
 		response.setStatus(HttpServletResponse.SC_OK);
 		baseRequest.setHandled(true);
@@ -32,31 +34,36 @@ public class StartJetty extends AbstractHandler {
 	}
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("SimTix Web Manager rel 0.2 June 11 - 12:37 AM");
-		Server server = new Server(8080);
 
-		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		context.setContextPath("/");
-		
-		
-		ResourceHandler resource_handler = new ResourceHandler();
-	    resource_handler.setDirectoriesListed(false);
-	    
-	    resource_handler.setResourceBase("./webcontent");
+	}
 
-	    HandlerList handlers = new HandlerList();
-	    handlers.setHandlers(new Handler[] { resource_handler, context });
-	   
-		context.addServlet(new ServletHolder(new JsHelper("JsHelper")), "/jshelper/*");
-		
-		server.setHandler(handlers);
-		
-		server.start();
-		server.join();
-		
-		// startar o banco
-		
-		// startar o navegador padrao apontando para a pagina inicial
+	@Override
+	public void run() {
+		try {
+			System.out.println("Ivella Offline");
+			Server server = new Server(8080);
+
+			ServletContextHandler context = new ServletContextHandler(
+					ServletContextHandler.SESSIONS);
+
+			ResourceHandler resource_handler = new ResourceHandler();
+			resource_handler.setDirectoriesListed(false);
+
+			resource_handler.setResourceBase("./webcontent");
+
+			HandlerList handlers = new HandlerList();
+			handlers.setHandlers(new Handler[] { resource_handler, context });
+
+			context.addServlet(new ServletHolder(new JsHelper("JsHelper")),
+					"/jshelper/*");
+
+			server.setHandler(handlers);
+			
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
