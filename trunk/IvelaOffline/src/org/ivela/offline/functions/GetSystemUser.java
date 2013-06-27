612338aa98c1a644baeeebaa974514c01274e747
jd.comment <jd.comment@337e1542-b571-11dd-8d2d-9519526c4844>
2013-06-27 18:05:06 +0000

    Browser function refactoring, avoiding use of swt

--- trunk/IvelaOffline/src/org/ivela/offline/functions/GetSystemUser.java
+++ trunk/IvelaOffline/src/org/ivela/offline/functions/GetSystemUser.java
@@ -4,35 +4,30 @@
  * Eldorado Confidential and Proprietary Information
  * Copyright (C)2010 Eldorado, All Rights Reserved
  *
- * Histórico de Modificações
+ * Histï¿½rico de Modificaï¿½ï¿½es
  *
- * Data           Quem              Descrição
+ * Data           Quem              Descriï¿½ï¿½o
  * ------------------------------------------------------------------------------------------------
  * 07 May 2010    Rafael Lagoa      (4115)Reajuste arquitetural.  
  */
-package br.org.eldorado.offline.browser.function;
+package org.ivela.offline.functions;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import org.eclipse.swt.browser.Browser;
-import org.eclipse.swt.browser.BrowserFunction;
+import org.ivela.offline.commons.ConnectionUtil;
+import org.ivela.offline.commons.LogWrapper;
+import org.ivela.offline.dao.SystemUserDAO;
+import org.ivela.offline.dao.SystemUserDAOImpl;
+import org.ivela.offline.domain.SystemUser;
+import org.ivela.offline.domain.SystemUserExample;
 
-import br.org.eldorado.offline.common.ConnectionUtil;
-import br.org.eldorado.offline.common.LogWrapper;
-import br.org.eldorado.offline.dao.SystemUserDAO;
-import br.org.eldorado.offline.dao.ibatis.SystemUserDAOImpl;
-import br.org.eldorado.offline.domain.SystemUser;
-import br.org.eldorado.offline.domain.SystemUserExample;
-
-public class GetSystemUser extends BrowserFunction {
+public class GetSystemUser implements BrowserFunction {
 
     private static LogWrapper logger = new LogWrapper(GetSystemUser.class);
     private static SystemUserDAO systemUserDao = new SystemUserDAOImpl(ConnectionUtil.getSqlMapClient());
 
-    public GetSystemUser (Browser browser, String name) {
-        super (browser, name);
-    }
+   
     public Object function (Object[] arguments) {
         SystemUserExample systemUserExample = new SystemUserExample();
         List<SystemUser> systemUsers = null;
