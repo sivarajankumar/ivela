7d142e30b0da7bcd2c5d0025cd253acb05b47c1c
jd.comment <jd.comment@337e1542-b571-11dd-8d2d-9519526c4844>
2013-07-13 00:37:10 +0000

    just refactoring

--- trunk/IvelaOffline/src/org/ivela/offline/dao/DbCommonTasks.java
+++ trunk/IvelaOffline/src/org/ivela/offline/dao/DbCommonTasks.java
@@ -5,12 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import br.com.infoserver.jotpcenter.util.exceptions.JOtpCenterException;
-
 public class DbCommonTasks {
-	protected void close(ResultSet rs, PreparedStatement ps, Connection con) throws JOtpCenterException {
-		if(rs!=null) try { rs.close(); } catch (SQLException e) { throw new JOtpCenterException(e); }
-		if(ps!=null) try { ps.close(); } catch (SQLException e) { throw new JOtpCenterException(e); }
-		if(con!=null) try { con.close(); } catch (SQLException e) { throw new JOtpCenterException(e); }
+	protected void close(ResultSet rs, PreparedStatement ps, Connection con) throws Exception {
+		if(rs!=null) try { rs.close(); } catch (SQLException e) { throw new Exception(e); }
+		if(ps!=null) try { ps.close(); } catch (SQLException e) { throw new Exception(e); }
+		if(con!=null) try { con.close(); } catch (SQLException e) { throw new Exception(e); }
 	}
 }
