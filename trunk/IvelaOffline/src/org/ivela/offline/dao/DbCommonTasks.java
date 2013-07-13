aba9029941ca9a9dc57e3ab75da6afd17ec3b503
jd.comment <jd.comment@337e1542-b571-11dd-8d2d-9519526c4844>
2013-07-13 01:50:09 +0000

    User DAO changes

--- trunk/IvelaOffline/src/org/ivela/offline/dao/DbCommonTasks.java
+++ trunk/IvelaOffline/src/org/ivela/offline/dao/DbCommonTasks.java
@@ -1,14 +1,53 @@
 package org.ivela.offline.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.ivela.offline.commons.Constants;
+import org.ivela.offline.utils.LoggerManager;
+
+
 public class DbCommonTasks {
-	protected void close(ResultSet rs, PreparedStatement ps, Connection con) throws Exception {
-		if(rs!=null) try { rs.close(); } catch (SQLException e) { throw new Exception(e); }
-		if(ps!=null) try { ps.close(); } catch (SQLException e) { throw new Exception(e); }
-		if(con!=null) try { con.close(); } catch (SQLException e) { throw new Exception(e); }
+	private static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+	private static String protocol = "jdbc:derby:";
+
+	/**
+	 * Conecta-se no banco
+	 * @return objeto conexao
+	 */
+	protected Connection getConnection(){
+		Connection conn = null;
+		try {
+			loadDriver();
+			String dbName = Constants.USER_DATA_FOLDER+"IVELA_DB"; // the name of the database
+			conn = DriverManager.getConnection(protocol + dbName + ";create=true");
+		} catch (SQLException e) {
+			LoggerManager.getInstance().logAtDebugTime("DbCommonTasks.class", "Problem when connecting: " + e.getMessage());
+		}
+		return conn;
 	}
+	
+	protected static void loadDriver() {
+		try {
+			Class.forName(driver).newInstance();
+			LoggerManager.getInstance().logAtDebugTime("CheckDB.class", "Loaded the appropriate driver");
+		} catch (ClassNotFoundException cnfe) {
+			LoggerManager.getInstance().logAtExceptionTime("CheckDB.class", "\nUnable to load the JDBC driver " + driver);
+			LoggerManager.getInstance().logAtExceptionTime("CheckDB.class", "Please check your CLASSPATH.");
+			cnfe.printStackTrace(System.err);
+		} catch (InstantiationException ie) {
+			LoggerManager.getInstance().logAtExceptionTime("CheckDB.class", "\nUnable to instantiate the JDBC driver "
+					+ driver);
+			ie.printStackTrace(System.err);
+		} catch (IllegalAccessException iae) {
+			LoggerManager.getInstance().logAtExceptionTime("CheckDB.class", "\nNot allowed to access the JDBC driver "
+					+ driver);
+			iae.printStackTrace(System.err);
+		}
+	}
+
+	
 }
--- trunk/IvelaOffline/src/org/ivela/offline/functions/GetSystemUser.java
+++ trunk/IvelaOffline/src/org/ivela/offline/functions/GetSystemUser.java
@@ -13,29 +13,29 @@
 package org.ivela.offline.functions;
 
 import java.sql.SQLException;
-import java.util.List;
 
-import org.ivela.offline.commons.ConnectionUtil;
-import org.ivela.offline.commons.LogWrapper;
 import org.ivela.offline.dao.SystemUserDAO;
 import org.ivela.offline.dao.SystemUserDAOImpl;
-import org.ivela.offline.domain.SystemUser;
-import org.ivela.offline.domain.SystemUserExample;
+import org.ivela.offline.utils.LoggerManager;
 
 public class GetSystemUser implements BrowserFunction {
 
-    private static LogWrapper logger = new LogWrapper(GetSystemUser.class);
-    private static SystemUserDAO systemUserDao = new SystemUserDAOImpl(ConnectionUtil.getSqlMapClient());
+	private static SystemUserDAO systemUserDao = new SystemUserDAOImpl();
 
-   
-    public Object function (Object[] arguments) {
-        SystemUserExample systemUserExample = new SystemUserExample();
-        List<SystemUser> systemUsers = null;
-        try {
-            systemUsers = systemUserDao.selectByExample(systemUserExample);
-        } catch (SQLException e) {
-            logger.error(e);
-        }
-        return systemUsers.get(0).getUsername();
-    }
+	public Object function(Object[] arguments) {
+		/*
+		 * SystemUserExample systemUserExample = new SystemUserExample();
+		 * List<SystemUser> systemUsers = null; try { systemUsers =
+		 * systemUserDao.selectByExample(systemUserExample); } catch
+		 * (SQLException e) { logger.error(e); } return
+		 * systemUsers.get(0).getUsername();
+		 */
+		try {
+			return systemUserDao.getUserName();
+		} catch (SQLException e) {
+			LoggerManager.getInstance().logAtDebugTime("GetSystemUser",
+					"Problem when getting user from DB: " + e.getMessage());
+		}
+		return null;
+	}
 }
